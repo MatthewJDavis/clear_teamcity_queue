d5a7011e47777f347cc2c2522a238728b69d9d13
MatthewJDavis <matthewdavis111@gmail.com>
2020-04-01 21:34:23 -0400

    update constants

--- clear_teamcity_queue/clear_teamcity_queue.py
+++ clear_teamcity_queue/clear_teamcity_queue.py
@@ -1,20 +1,20 @@
 import os
 from helpers import request_teamcity, check_for_agent, get_queue_info
 
-baseUrl = 'http://localhost:8111/app/rest'
-queueUrl = f'{baseUrl}/buildQueue'
-token = os.getenv('TOKEN')
-headers = {
-    'Authorization': f'Bearer {token}',
+BASEURL = 'http://localhost:8111/app/rest'
+QUEUEURL = f'{BASEURL}/buildQueue'
+TOKEN = os.getenv('TOKEN')
+HEADERS = {
+    'Authorization': f'Bearer {TOKEN}',
     'Content-Type': 'application/xml',
 }
 
-queueList = get_queue_info(queueUrl=queueUrl, headers= headers)
+queueList = get_queue_info(queueUrl=QUEUEURL, headers=HEADERS)
 
 if len(queueList.builds) > 0:
     for buildInfo in queueList.builds.build:
         removed = check_for_agent(
-            queueUrl=queueUrl, buildId=buildInfo['id'], headers=headers)
+            queueUrl=QUEUEURL, buildId=buildInfo['id'], headers=HEADERS)
         print(removed)
 else:
     print('No builds in the queue')
