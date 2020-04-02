f6f4af62766ffc4b53ed855f16219c689e60c9a9
MatthewJDavis <matthewdavis111@gmail.com>
2020-04-01 21:36:26 -0400

    update var, remove import

--- clear_teamcity_queue/clear_teamcity_queue.py
+++ clear_teamcity_queue/clear_teamcity_queue.py
@@ -1,5 +1,5 @@
 import os
-from helpers import request_teamcity, check_for_agent, get_queue_info
+from helpers import check_for_agent, get_queue_info
 
 BASEURL = 'http://localhost:8111/app/rest'
 QUEUEURL = f'{BASEURL}/buildQueue'
@@ -9,10 +9,10 @@ HEADERS = {
     'Content-Type': 'application/xml',
 }
 
-queue_list = get_queue_info(queueUrl=QUEUEURL, headers=HEADERS)
+QUEUE = get_queue_info(queueUrl=QUEUEURL, headers=HEADERS)
 
-if len(queue_list.builds) > 0:
-    for buildInfo in queue_list.builds.build:
+if len(QUEUE.builds) > 0:
+    for buildInfo in QUEUE.builds.build:
         removed = check_for_agent(
             queueUrl=QUEUEURL, buildId=buildInfo['id'], headers=HEADERS)
         print(removed)
