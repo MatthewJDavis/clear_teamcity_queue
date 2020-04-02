57fe3ff9e6f47e4fde28e1594ee66b2edfc82a60
MatthewJDavis <matthewdavis111@gmail.com>
2020-04-01 22:00:30 -0400

    update var names

--- clear_teamcity_queue/clear_teamcity_queue.py
+++ clear_teamcity_queue/clear_teamcity_queue.py
@@ -1,20 +1,20 @@
 import os
 from helpers import check_for_agent, get_queue_info
 
-BASEURL = 'http://localhost:8111/app/rest'
-QUEUEURL = f'{BASEURL}/buildQueue'
+BASE_URL = 'http://localhost:8111/app/rest'
+QUEUE_URL = f'{BASE_URL}/buildQueue'
 TOKEN = os.getenv('TOKEN')
 HEADERS = {
     'Authorization': f'Bearer {TOKEN}',
     'Content-Type': 'application/xml',
 }
 
-QUEUE = get_queue_info(queueUrl=QUEUEURL, headers=HEADERS)
+QUEUE = get_queue_info(queue_url=QUEUE_URL, headers=HEADERS)
 
 if len(QUEUE.builds) > 0:
     for buildInfo in QUEUE.builds.build:
         removed = check_for_agent(
-            queueUrl=QUEUEURL, buildId=buildInfo['id'], headers=HEADERS)
+            queue_url=QUEUE_URL, buildId=buildInfo['id'], headers=HEADERS)
         print(removed)
 else:
     print('No builds in the queue')
