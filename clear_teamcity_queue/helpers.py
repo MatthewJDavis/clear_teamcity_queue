280c006de51306596061d6edf95f49a20ad0bac0
MatthewJDavis <matthewdavis111@gmail.com>
2020-04-01 22:00:47 -0400

    var name update and line length

--- clear_teamcity_queue/helpers.py
+++ clear_teamcity_queue/helpers.py
@@ -14,16 +14,19 @@ def request_teamcity(url, headers):
     return response
 
 
-def get_queue_info(queueUrl, headers):
-    response = request_teamcity(queueUrl, headers)
+def get_queue_info(queue_url, headers):
+    response = request_teamcity(queue_url, headers)
     xml = response.content.decode()
     return untangle.parse(xml)
 
+
 # remove from queue
-def remove_builds(queueUrl, buildId, headers):
+def remove_builds(queue_url, buildId, headers):
     try:
-        data = "<buildCancelRequest comment='No available agents to run build.' readdIntoQueue='false' />"
-        buildUrl = f'{queueUrl}/id:{buildId}'
+        data = "<buildCancelRequest \
+                comment = 'No available agents to run build.'\
+                readdIntoQueue='false' />"
+        buildUrl = f'{queue_url}/id:{buildId}'
         response = requests.post(buildUrl, data=data, headers=headers)
         response.raise_for_status()
         return response
@@ -32,13 +35,15 @@ def remove_builds(queueUrl, buildId, headers):
     except Exception as err:
         print(f'Another error occurred {err}')
 
+
 # Check if build has an agent and delete if not
-def check_for_agent(queueUrl, buildId, headers):
-    agentUrl = f'{queueUrl}/id:{buildId}/compatibleAgents'
+def check_for_agent(queue_url, buildId, headers):
+    agentUrl = f'{queue_url}/id:{buildId}/compatibleAgents'
     agentInfo = request_teamcity(agentUrl, headers)
     xml = agentInfo.content.decode()
     agent = untangle.parse(xml)
     if agent.agents['count'] == '0':
-        response = remove_builds(queueUrl, buildId, headers)
+        response = remove_builds(queue_url, buildId, headers)
         removed = untangle.parse(response.content.decode())
-        return (f"No available agents to run build. Removed build: {removed.build.buildType['webUrl']}")
+        return f"No available agents to run build. Removed build:\
+        {removed.build.buildType['webUrl']}"
