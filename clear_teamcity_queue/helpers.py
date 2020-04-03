cc7098314ca4ebd42ff98f566e0b961976234e2c
MatthewJDavis <matthewdavis111@gmail.com>
2020-04-02 20:55:12 -0400

    pylint improvements

--- clear_teamcity_queue/helpers.py
+++ clear_teamcity_queue/helpers.py
@@ -4,24 +4,24 @@ import untangle
 
 
 def request_teamcity(url, headers):
+    ''' Helper function to connect to TeamCity API '''
     try:
         response = requests.get(url, headers=headers)
         response.raise_for_status()
     except HTTPError as http_err:
         print(f'HTTP error occurred: {http_err}')
-    except Exception as err:
-        print(f'Another error occurred: {err}')
     return response
 
 
 def get_queue_info(queue_url, headers):
+    ''' Returns TeamCity queue information as parsed XML '''
     response = request_teamcity(queue_url, headers)
     xml = response.content.decode()
     return untangle.parse(xml)
 
 
-# remove from queue
 def remove_builds(queue_url, build_id, headers):
+    ''' Removes builds when no agents available '''
     try:
         data = "<buildCancelRequest \
                 comment = 'No available agents to run build.'\
@@ -32,12 +32,10 @@ def remove_builds(queue_url, build_id, headers):
         return response
     except HTTPError as http_err:
         print(f'HTTP error occurred: {http_err}')
-    except Exception as err:
-        print(f'Another error occurred {err}')
 
 
-# Check if build has an agent and delete if not
 def check_for_agent(queue_url, build_id, headers):
+    ''' Checks to see if the build has agents to run on '''
     agent_url = f'{queue_url}/id:{build_id}/compatibleAgents'
     agent_info = request_teamcity(agent_url, headers)
     xml = agent_info.content.decode()
@@ -45,6 +43,5 @@ def check_for_agent(queue_url, build_id, headers):
     if agent.agents['count'] == '0':
         response = remove_builds(queue_url, build_id, headers)
         removed = untangle.parse(response.content.decode())
-        return f"No available agents to run build. Removed build:\
-        {removed.build.buildType['webUrl']}"
+        return f"Removed build: {removed.build.buildType['webUrl']}"
     return 'Build has agents to run on.'
